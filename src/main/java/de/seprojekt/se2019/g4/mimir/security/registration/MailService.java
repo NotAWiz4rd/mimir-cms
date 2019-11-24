d5caf49e0c78eb6dc3b0e40e3aad5129cbe9c8e1
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2019-11-24 11:31:57 +0100

    Add MailService POC

---
+++ src/main/java/de/seprojekt/se2019/g4/mimir/security/registration/MailService.java
@@ -0,0 +1,23 @@
+package de.seprojekt.se2019.g4.mimir.security.registration;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    private JavaMailSender javaMailSender;
+
+    public MailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendMail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        this.javaMailSender.send(message);
+    }
+}
