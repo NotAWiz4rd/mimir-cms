bc5503d0572ceef9237874d9cf580962c0d9248a
schneefux <schneefux+commit@schneefux.xyz>
2019-12-06 15:03:15 +0100

    Add JWT authentication and authorization

---
+++ src/main/java/de/seprojekt/se2019/g4/mimir/security/JwtAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package de.seprojekt.se2019.g4.mimir.security;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager,
+        ApplicationContext ctx) {
+        this.authenticationManager = authenticationManager;
+        setFilterProcessesUrl("/login");
+        this.jwtTokenProvider = ctx.getBean(JwtTokenProvider.class);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        var username = request.getParameter("username");
+        var password = request.getParameter("password");
+        var authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain filterChain, Authentication authentication) {
+        var token = jwtTokenProvider.generateToken(authentication);
+        response.addHeader("Authorization", "Bearer " + token);
+    }
+}
---
+++ src/main/java/de/seprojekt/se2019/g4/mimir/security/JwtAuthorizationFilter.java
@@ -0,0 +1,60 @@
+package de.seprojekt.se2019.g4.mimir.security;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+  private JwtTokenProvider jwtTokenProvider;
+
+  public JwtAuthorizationFilter(AuthenticationManager authenticationManager,
+    ApplicationContext ctx) {
+      super(authenticationManager);
+      this.jwtTokenProvider = ctx.getBean(JwtTokenProvider.class);
+  }
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                  FilterChain filterChain) throws IOException, ServletException {
+      var authentication = getAuthentication(request);
+      if (authentication == null) {
+          filterChain.doFilter(request, response);
+          return;
+      }
+
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+      filterChain.doFilter(request, response);
+  }
+
+  private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+      var token = request.getHeader("Authorization");
+
+      if (token == null) {
+        return null;
+      }
+
+      if (!token.startsWith("Bearer ")) {
+        return null;
+      }
+
+      token = token.replace("Bearer ", "");
+
+      if (!jwtTokenProvider.validateToken(token)) {
+        return null;
+      }
+
+      var username = jwtTokenProvider.getUserName(token);
+      return new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+  }
+}
