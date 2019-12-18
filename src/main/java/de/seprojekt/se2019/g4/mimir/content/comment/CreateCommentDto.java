7881088b47ec2098dca8dbab8d553d755858c24a
schneefux <schneefux+commit@schneefux.xyz>
2019-12-18 18:49:51 +0100

    Add comment controller (#21)
    * Add comment controller
    
    * Update readme with comment endpoints
    
    * Comment controller: Add authorization
    
    * Comments: Refer to user object
    
    * Comments: Add deletion

---
+++ src/main/java/de/seprojekt/se2019/g4/mimir/content/comment/CreateCommentDto.java
@@ -0,0 +1,49 @@
+package de.seprojekt.se2019.g4.mimir.content.comment;
+
+import java.util.Objects;
+
+import javax.validation.constraints.NotNull;
+
+public class CreateCommentDto {
+    @NotNull
+    private Long artifactId;
+
+    @NotNull
+    private String text;
+
+    public Long getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(Long artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return Objects.equals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(artifactId, text);
+    }
+
+    @Override
+    public String toString() {
+        return "CreateCommentDto{" +
+                ", artifactId=" + artifactId +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
+
+}
