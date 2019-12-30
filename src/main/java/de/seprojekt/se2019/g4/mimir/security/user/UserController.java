ab92cbd5a3bafbea53f4e56d150892517d93df04
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2019-12-30 13:09:00 +0100

    Improve /user endpoint's robustness

--- src/main/java/de/seprojekt/se2019/g4/mimir/security/user/UserController.java
+++ src/main/java/de/seprojekt/se2019/g4/mimir/security/user/UserController.java
@@ -2,6 +2,7 @@ package de.seprojekt.se2019.g4.mimir.security.user;
 
 import de.seprojekt.se2019.g4.mimir.security.JwtPrincipal;
 import java.security.Principal;
+import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,13 +25,20 @@ public class UserController {
    */
   @GetMapping(value = "/user")
   public ResponseEntity<User> getUser(Principal principal) {
-    if (JwtPrincipal.fromPrincipal(principal)
-        .isAnonymous()) { // return fake Guest user for the Frontend ot use
+    JwtPrincipal jwtPrincipal = JwtPrincipal.fromPrincipal(principal);
+
+    if (jwtPrincipal.isAnonymous()) {// return fake Guest user for the Frontend ot use
       User anonymous = new User();
       anonymous.setName("Guest");
       anonymous.setId(-1L);
-      return ResponseEntity.ok().body(new User());
+      return ResponseEntity.ok().body(anonymous);
     }
-    return ResponseEntity.ok().body(userService.findByName(principal.getName()).get());
+
+    Optional<User> userOptional = userService.findByName(jwtPrincipal.getName());
+    if (userOptional.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok().body(userOptional.get());
   }
 }
