128f532b595434a2bd2b4ef2aea7cb7076ae0c02
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2020-01-02 20:27:44 +0100

    Enable login using mail address

--- src/main/java/de/seprojekt/se2019/g4/mimir/security/JwtAuthenticationFilter.java
+++ src/main/java/de/seprojekt/se2019/g4/mimir/security/JwtAuthenticationFilter.java
@@ -1,8 +1,11 @@
 package de.seprojekt.se2019.g4.mimir.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.seprojekt.se2019.g4.mimir.security.user.User;
+import de.seprojekt.se2019.g4.mimir.security.user.UserService;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Optional;
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,12 +20,14 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
   private final AuthenticationManager authenticationManager;
 
   private JwtTokenProvider jwtTokenProvider;
+  private UserService userService;
 
   public JwtAuthenticationFilter(AuthenticationManager authenticationManager,
       ApplicationContext ctx) {
     this.authenticationManager = authenticationManager;
     setFilterProcessesUrl("/login");
     this.jwtTokenProvider = ctx.getBean(JwtTokenProvider.class);
+    this.userService = ctx.getBean(UserService.class);
   }
 
   @Override
@@ -30,6 +35,14 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
       HttpServletResponse response) {
     var username = request.getParameter("username");
     var password = request.getParameter("password");
+
+    if (username != null && username.contains("@")) {
+      Optional<User> userOptional = userService.findByMail(username);
+      if (userOptional.isPresent()) {
+        username = userOptional.get().getName();
+      }
+    }
+
     var authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
 
     return authenticationManager.authenticate(authenticationToken);
