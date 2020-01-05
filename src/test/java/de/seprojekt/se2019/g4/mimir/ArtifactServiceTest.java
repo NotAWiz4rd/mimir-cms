fc708efeeaa769a6d3087a19b2474b51890e824e
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2020-01-05 19:19:09 +0100

    Add some tests

---
+++ src/test/java/de/seprojekt/se2019/g4/mimir/ArtifactServiceTest.java
@@ -0,0 +1,71 @@
+package de.seprojekt.se2019.g4.mimir;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import de.seprojekt.se2019.g4.mimir.content.artifact.Artifact;
+import de.seprojekt.se2019.g4.mimir.content.artifact.ArtifactService;
+import de.seprojekt.se2019.g4.mimir.content.space.Space;
+import de.seprojekt.se2019.g4.mimir.security.user.User;
+import de.seprojekt.se2019.g4.mimir.security.user.UserService;
+import java.io.IOException;
+import javax.transaction.Transactional;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class ArtifactServiceTest {
+
+  @Autowired
+  UserService userService;
+
+  @Autowired
+  ArtifactService artifactService;
+
+  private User user;
+  private Space space;
+
+  @Before
+  public void init() {
+    user = userService.findByName("thellmann").get();
+    space = user.getSpaces().get(0);
+  }
+
+  @Test
+  public void shouldCreateArtifact() throws IOException {
+    String name = "file1.txt";
+    Artifact artifact = artifactService.create(
+        name,
+        new MockMultipartFile(name, name, "text/plain", "foobar".getBytes()),
+        space.getRootFolder()
+    );
+    assertNotNull("should exist", artifact);
+    assertEquals("should have name", name, artifact.getName());
+    assertEquals("should have parent folder", space.getRootFolder(), artifact.getParentFolder());
+    assertEquals("should have space", space, artifact.getSpace());
+    assertEquals("should have content type", MediaType.valueOf("text/plain"), artifact.getContentType());
+  }
+
+  @Test
+  public void shouldDeleteArtifact() throws IOException {
+    String name = "file1.txt";
+    Artifact artifact = artifactService.create(
+        name,
+        new MockMultipartFile(name, name, "text/plain", "foobar".getBytes()),
+        space.getRootFolder()
+    );
+    artifactService.delete(artifact);
+    assertTrue("should not exist", artifactService.findById(artifact.getId()).isEmpty());
+  }
+
+}
---
+++ src/test/java/de/seprojekt/se2019/g4/mimir/SpaceServiceTest.java
@@ -0,0 +1,65 @@
+package de.seprojekt.se2019.g4.mimir;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import de.seprojekt.se2019.g4.mimir.content.folder.Folder;
+import de.seprojekt.se2019.g4.mimir.content.folder.FolderService;
+import de.seprojekt.se2019.g4.mimir.content.space.Space;
+import de.seprojekt.se2019.g4.mimir.content.space.SpaceService;
+import de.seprojekt.se2019.g4.mimir.security.JwtPrincipal;
+import de.seprojekt.se2019.g4.mimir.security.user.User;
+import de.seprojekt.se2019.g4.mimir.security.user.UserService;
+import javax.transaction.Transactional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class SpaceServiceTest {
+
+  @Autowired
+  UserService userService;
+
+  @Autowired
+  FolderService folderService;
+
+  @Autowired
+  SpaceService spaceService;
+
+  @Test
+  public void shouldCreateSpace() {
+    String spaceName = "MyTestSpace";
+    Space newSpace = spaceService.create(spaceName, new JwtPrincipal("thellmann"));
+    Folder rootFolder = newSpace.getRootFolder();
+    User user = userService.findByName("thellmann").get();
+    assertNotNull("should exist", newSpace);
+    assertNotNull("should have root folder", rootFolder);
+    assertEquals("should have name", spaceName, newSpace.getName());
+    assertTrue("user should be member of space", user.getSpaces().contains(newSpace));
+  }
+
+  @Test
+  public void shouldDeleteSpace() {
+    String spaceName = "MyTestSpace";
+    Space newSpace = spaceService.create(spaceName, new JwtPrincipal("thellmann"));
+    Folder rootFolder = newSpace.getRootFolder();
+    Folder testFolder = folderService.create(newSpace.getRootFolder(), "spaceTestFolder");
+
+    spaceService.delete(newSpace);
+    User user = userService.findByName("thellmann").get();
+
+    assertTrue("should not exist", spaceService.findById(newSpace.getId()).isEmpty());
+    assertTrue("root folder should not exist", folderService.findById(rootFolder.getId()).isEmpty());
+    assertTrue("child folder should not exist", folderService.findById(testFolder.getId()).isEmpty());
+    assertFalse("user should not be member of space", user.getSpaces().contains(newSpace));
+  }
+
+
+}
